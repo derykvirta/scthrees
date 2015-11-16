0f512d43a20ca7bf6cfb82a93f07c951fc1382c9
Deryk DeGuzman <deryk.deguzman@gmail.com>
2015-11-15 21:00:26 -0800

    add all files and dirs

---
+++ index.php
@@ -0,0 +1,93 @@
+<html>
+  <head>
+    <title>Curry 3 Count | How many 3s has Steph Curry made this season?</title>
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="static/js/sc3s.min.js?v=3"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-69863626-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+    <script type="text/javascript" async src="https://platform.twitter.com/widgets.js"></script>
+    <?php
+      // Get cached stats for twitter card.
+      $stats = file_get_contents('data/s.txt');
+      list($threes, $threes_attempted, $games, $next) = explode(',', $stats);
+      $pace = intval(intval($threes)/intval($games)*82);
+      $twitter_card_description = 'Curry3Count: ' . $threes . ' 3s/' . $games . ' gms, on a season pace of ' . $pace .'.';
+      $twitter_intent_url = 'https:\/\/twitter.com\/intent\/tweet?text=' . urlencode($twitter_card_description) . '&button_hashtag=#curry3count&hashtags=NBA,DubNation,LetsGoWarriors&url=http%3A%2F%2Fscthrees.com%2F';
+
+      // Generate random number for cache busting twitter card image.
+      $rand = rand(0, 9999);
+      $statscard_url = 'http://' . $_SERVER['HTTP_HOST'] . '/statscard.png?v=' . $rand;
+    ?>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+    <link href='static/css/sc3s.css?v=3' rel='stylesheet' type='text/css'>
+    <link rel="shortcut icon" href="favicon.ico" id="page_favicon" type="image/x-icon"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@deryk84" />
+    <meta name="twitter:title" content="Curry3Count | How many 3s has Steph Curry made this season?" />
+    <meta name="twitter:description" content="<?php echo $twitter_card_description ?>" />
+    <meta name="twitter:image" content="<?php echo $statscard_url ?>" />
+  </head>
+  <body>
+    <div id="c">
+      <div id="c-sub">
+        <img id="sc-img1" src="static/img/sc3s2.png" /><div id="r-mtr">
+          <div id="rcrd"><div class="t-num">286</div></div>
+          <div id="curr">
+            <div id="curr-indctr"><div class="t-num">38</div></div>
+          </div>
+        </div>
+        <div id="s-c">
+          <div id="m">
+            <div id="m-l">
+              <div class="d" id="l">
+                <div id="spinner" class="animate-flicker"></div><span>LIVE</span>
+              </div>
+              <div class="d" id="nl">
+                <span>NEXT: <span id="ng"></span></span>
+              </div>
+            </div>
+            <div id="threes" class="stat-container">
+              <span></span> <span id="3ptrs-label">3 PTRS</span>
+              <span class="spacer"> </span>
+            </div>
+            <div id="games" class="stat-container">
+              <span></span> GAMES
+              <span class="spacer"> </span>
+            </div>
+            <div id="pace" class="stat-container">
+              <span></span> PACE
+              <span class="spacer"> </span>
+            </div>
+            <div id="pct" class="stat-container">
+              <span></span> 3PT%
+              <span class="spacer"> </span>
+            </div>
+          </div>
+          <div id="sc-img2-c"><img id="sc-img2" src="static/img/sc3s2.png"></div>
+        </div>
+      </div>
+      <div id="f-ctr">
+        <div id="f">#CURRY3COUNT | SCTHREES.COM</div>
+        <div id="tweet-button-ctr">
+          <a style="display: none;" class="twitter-hashtag-button"
+             href="<?php echo $twitter_intent_url ?>" >
+            Tweet #curry3count
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <script>
+      sc3s.init();
+    </script>
+  </body>
+</html>
