f4e64a7557d07c4f369843327f5b9def5fd44a51
Deryk DeGuzman <deryk.deguzman@gmail.com>
2015-11-19 13:34:23 -0800

    WIP

--- flask_rest_service/__init__.py
+++ flask_rest_service/__init__.py
@@ -5,11 +5,13 @@ from flask.ext.pymongo import PyMongo
 from flask import make_response
 from bson.json_util import dumps
 
-MONGO_URL = 'mongodb://heroku_nkg65vjs:dm0eagj237ofa7ghgnb63suih2@ds057234.mongolab.com:57234/heroku_nkg65vjs'
+MONGO_URL = os.environ.get('MONGOLAB_URI')
+if not MONGO_URL:
+    MONGO_URL = 'mongodb://heroku_nkg65vjs:dm0eagj237ofa7ghgnb63suih2@ds057234.mongolab.com:57234/heroku_nkg65vjs'
 
 app = Flask(__name__)
 
-app.config['MONGOLAB_URI'] = MONGO_URL
+app.config['MONGO_URI'] = MONGO_URL
 mongo = PyMongo(app)
 
 def output_json(obj, code, headers=None):
